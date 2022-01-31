f1512e68e921e4e02b789b212c12e161b7e89abf
jacob gishwiller <sirbruce13@yahoo.com>
2022-01-31 13:53:36 -0700

    first commit

---
+++ AccountTest/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+   class Account
+   {
+      private string name; // instance variable
+
+      // method that sets the account name in the object
+      public void SetName(string accountName)
+      {
+         name = accountName; //store the account name
+      }
+
+      //method that retrieves the account name from the object
+      public string GetName()
+      {
+         return name; // returns name's value to this method's caller
+      }
+
+   }
+}
