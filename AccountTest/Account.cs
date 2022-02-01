ad174826d645dfe6628d64d17fc5a7275bcffda3
jacob gishwiller <sirbruce13@yahoo.com>
2022-02-01 08:53:55 -0700

    properties

---
+++ AccountTest/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+   class Account
+   {
+      private string name; // instance variable
+
+      //property to get and set the name instance variable
+      public string Name
+      {
+         get //returns the corresponding instance variables value
+         {
+            return name; //returns the value of name to the client code
+         }
+         set //assigns a new value to the corresponding instance variable 
+         {
+            name = value; //value is implicitly declared and initialized 
+         }
+
+      }
+   }
+}
