f3fde13271cafca753943c3d7d9303cf8f9f50d2
jacob gishwiller <sirbruce13@yahoo.com>
2022-02-01 13:37:22 -0700

    almost complete version

--- AccountTest/AccountTest.cs
+++ AccountTest/AccountTest.cs
@@ -7,12 +7,32 @@ namespace Bank
       static void Main(string[] args)
       {
          //create two Account objects
-         Account account1 = new Account("Jane Green");
-         Account account2 = new Account("John Blue");
+         Account account1 = new Account("Jane Green", 50.00m);
+         Account account2 = new Account("John Blue", -7.53m);
 
          //display intial value of name or each Account
-         Console.WriteLine($"account1 name is: {account1.Name}");
-         Console.WriteLine($"account2 name is {account2.Name}");
+         Console.WriteLine($"{account1.Name}'s balance: {account1.Balance:C}");
+         Console.WriteLine($"{account2.Name}'s balance: {account2.Balance:C}");
+
+         //prompt for than read input
+         Console.Write("\nEnter deposit amount for account1 ");
+         decimal depositAmount = decimal.Parse(Console.ReadLine());
+         Console.WriteLine($"adding {depositAmount:C} to account1 balance\n");
+         //Add to account1's balance
+
+         //display balance
+         Console.WriteLine($"{account1.Name}'s balance: {account1.Balance:C}");
+         Console.WriteLine($"{ account2.Name}'s balance {account2.Balance:C}");
+
+         //prompt for then read input
+         Console.Write("\nEnter depsoit amount for account2: ");
+         depositAmount = decimal.Parse(Console.ReadLine());
+         Console.WriteLine($"adding {depositAmount:C}"); // add to account2's balance
+
+         //display balances
+         Console.WriteLine($"{account1.Name}'s balance: {account1.Balance:C}");
+         Console.WriteLine($"{account2.Name}'s balance: {account2.Balance:C}");
+
 
       }
    }
---
+++ AccountTest/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountTest
+{
+   class Account
+   {
+      public string Name { get; set; } //auto-implemented property
+      private decimal Balance; //instance variable
+
+      // Account constructor that receives two parameters
+      public Account(string accountName, decimal initialBalance)
+      {
+         Name = accountName;
+         Balance = initialBalance; // Balance's set accessor validates
+      }
+
+      //Balance property with validation
+      public decimal Balance
+      {
+         get
+         {
+            return Balance;
+         }
+         private set //can be used only within the class
+         {
+             //validate that the balance is greater that 0.0; if it's not,
+             //instance variable balance keeps it's prior value 
+            if (value > 0.0m) // m indicates that 0.0 is a decimal literal
+            {
+               Balance = value;
+            } 
+         
+         }
+      }
+
+      //method that deposits (adds) only a valid amount to the balance
+      public void Deposit(decimal depositAmount)
+      {
+         if (depositAmount > 0.0m)  // if the depositAmount is valid
+         {
+            Balance = Balance + depositAmount; // add it to the balance
+         }
+      }
+   }
+}
